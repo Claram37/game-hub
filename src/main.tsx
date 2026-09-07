8e8bf8780f5e2990ebe9d383007beee9d3b3b9dc
Claram37 <123749352+Claram37@users.noreply.github.com>
2026-09-07 15:31:42 +0300

    Install Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import type { ReactNode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
-createRoot(document.getElementById('root')!).render(
+const AppComponent = App as unknown as () => ReactNode;
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={defaultSystem}>
+      <AppComponent />
+    </ChakraProvider>
   </StrictMode>,
-)
+);

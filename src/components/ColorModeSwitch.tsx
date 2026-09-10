8d191222cf233c5252b62c391e41497c8cb6a08f
Claram37 <123749352+Claram37@users.noreply.github.com>
2026-09-10 14:44:28 +0300

    implement theme toggle in ColorModeSwitch component and install bootstrap icons

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,13 +1,35 @@
 import { Form } from "react-bootstrap";
 
+import { useEffect, useState } from "react";
+
 const ColorModeSwitch = () => {
+  // Initialize the default state to be light mode
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "light";
+  });
+
+  // Apply the theme to the HTML tag when the state changes
+  useEffect(() => {
+    document.documentElement.setAttribute("data-bs-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  // Handle the toggling logic on click
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
-    <Form.Check
-      type="switch"
-      id="color-mode-switch"
-      label="Dark Mode"
-      className="text-nowrap"
-    />
+    <>
+      <Form.Check
+        type="switch"
+        id="color-mode-switch"
+        label="Dark Mode"
+        onClick={toggleTheme}
+        style={{ fontSize: "14px" }}
+        checked={theme === "dark"}
+      />
+    </>
   );
 };
 

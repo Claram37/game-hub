ec00b189541ccefb7ebed6b73edf302b38a64312
Claram37 <123749352+Claram37@users.noreply.github.com>
2026-09-11 14:25:32 +0300

    Add the games data and pass the data to the game cards to create the games grid

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,25 +1,37 @@
 import { Card } from "react-bootstrap";
+import noImage from "../assets/no-image-placeholder.webp";
+
 // import { Game } from '../hooks/useGames'
 
 import CriticScore from "./CriticScore";
+import type { Platform } from "./PlatformSelector";
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+  rating: number;
+}
+interface Props {
+  game: Game;
+}
 
-// interface Props {
-//   game: Game
-// }
+const getCroppedImageUrl = (url: string) => {
+  if (!url) return noImage; // empty image
+  return url;
+};
 
-const GameCard = () => {
+const GameCard = ({ game }: Props) => {
   return (
     <Card className="overflow-hidden">
-      <Card.Img
-        variant="top"
-        src="https://media.rawg.io/media/games/26d/26d4437715bee60138dab4a7c8c59c92.jpg"
-      />
+      <Card.Img variant="top" src={getCroppedImageUrl(game.background_image)} />
       <Card.Body className="p-4">
         <div className="d-flex align-items-center justify-content-between mb-3">
           <CriticScore />
         </div>
         <Card.Title as="h2" className="fs-4 fw-semibold mb-0">
-          Game name
+          {game.name}
         </Card.Title>
       </Card.Body>
     </Card>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,11 +1,16 @@
 import GameCard from "./GameCard";
+import results from "../data/games";
 
 const GameGrid = () => {
+  const games = results.flatMap((result) => result.results); // map the results array to initialize the games
+
   return (
     <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4 p-2">
-      <div className="col">
-        <GameCard />
-      </div>
+      {games.map((game) => (
+        <div key={game.id} className="col">
+          <GameCard game={game} />
+        </div>
+      ))}
     </div>
   );
 };

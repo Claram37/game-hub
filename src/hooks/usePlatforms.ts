c3f8038440d9e9c5ba26b8c189481d6f1ec6718d
Claram37 <123749352+Claram37@users.noreply.github.com>
2026-09-22 14:30:03 +0300

    Refactor usePlatforms hook to utilize React Query for data fetching

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,13 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
 import platform from "../data/platform";
+import apiClient from "../services/api-client";
+import type { FetchResponse } from "./useData";
 
 export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
-const usePlatforms = () => ({
-  data: platform,
-  isLoading: false,
-  error: null,
-});
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data.results),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hrs
+    initialData: platform,
+  });
 export default usePlatforms;

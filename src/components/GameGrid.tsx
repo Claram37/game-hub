8adebc87c9d2783397de4c7ea83067924cd05cd1
Claram37 <123749352+Claram37@users.noreply.github.com>
2026-09-22 17:17:02 +0300

    Add react-infinite-scroll-component for infinite scrolling in GameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,27 +3,30 @@ import useGames from "../hooks/useGames";
 import GameCardSkeleton from "./GameCardSkeleton";
 import type { GameQuery } from "../App";
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Spinner } from "react-bootstrap";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const {
-    data,
-    error,
-    isLoading,
-    isFetchingNextPage,
-    fetchNextPage,
-    hasNextPage,
-  } = useGames(gameQuery);
+  const { data, error, isLoading, fetchNextPage, hasNextPage } =
+    useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
   if (error) return <p className="text-danger px-2">{error.message}</p>;
 
+  const fetchedGamesCount =
+    data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+
   return (
-    <>
+    <InfiniteScroll
+      dataLength={fetchedGamesCount}
+      hasMore={!!hasNextPage}
+      next={() => fetchNextPage()}
+      loader={<Spinner />}
+    >
       <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4 p-2">
         {isLoading &&
           skeletons.map((skeleton) => (
@@ -42,16 +45,17 @@ const GameGrid = ({ gameQuery }: Props) => {
             </React.Fragment>
           ))}
       </div>
-      {hasNextPage && (
+    </InfiniteScroll>
+  );
+};
+
+export default GameGrid;
+
+/* {hasNextPage && (
         <Button
           className="my-3 btn btn-secondary m-2"
           onClick={() => fetchNextPage()}
         >
           {isFetchingNextPage ? "Loading..." : "Load More"}
         </Button>
-      )}
-    </>
-  );
-};
-
-export default GameGrid;
+      )} */
